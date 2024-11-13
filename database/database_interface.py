a053b39abcf8b5446bc17983bb12408c4e56ca04
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2024-11-12 21:20:07 -0500

    Moved initialize_database to database_interface, added database init to main.py, and added config.ini.

--- database/database_interface.py
+++ database/database_interface.py
@@ -1,14 +1,26 @@
+import configparser
 import psycopg2
+
+config_parser = configparser.ConfigParser(allow_no_value=True)
+config_parser.read('config.ini')
+
+initialize_database_sql = config_parser.get('FILE_PATHS', 'initialize_database')
 
 class database_interface:
 
-    def __init__(self, dbname: str = None, username: str = None, password: str = None, host: str = None, port: str = None):
+    # All positional arguments are for deliniating which database to connect to and how.
+    def __init__(self, dbname: str = None, username: str = None, 
+                 password: str = None, host: str = None, port: str = None):
         self.conn = psycopg2.connect(dbname=dbname, user=username, password=password, host=host, port=port)
         self.curr = self.conn.cursor()
 
     def __del__(self):
         self.curr.close()
         self.conn.close()
+
+    def initialize_database(self):
+        with open(initialize_database_sql) as query:
+            self.exec_query(query.read())
 
 
     def exec_query(self, query: str, fetch: bool = False, *inputs):
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+import os
+import configparser
+
+from database.database_interface import database_interface
+
 from server.server import activate_server
 from server.amqp_data_extractor import amqp_data_extractor
 from server.data_outputter import data_printer
 
+config_parser = configparser.ConfigParser(allow_no_value=True)
+config_parser.read('config.ini')
+
+DBNAME = config_parser.get('DATABASE_ACCESS', 'DBNAME')
+USERNAME = config_parser.get('DATABASE_ACCESS', 'USERNAME')
+PASSWORD = config_parser.get('DATABASE_ACCESS', 'PASSWORD')
+HOST = config_parser.get('DATABASE_ACCESS', 'HOST')
+PORT = config_parser.get('DATABASE_ACCESS', 'PORT')
+
+INIT_DB = config_parser.get('STARTUP_PARAMETERS', 'INIT_DB')
+
 def main():
-    # activate_server()
+
+    # Create application-specific objects
     outputter = data_printer()
     extractor = amqp_data_extractor(outputter)
+    interface = database_interface(dbname=DBNAME, username=USERNAME, password=PASSWORD, host=HOST, port=PORT)
 
-    extractor.extract_data()
+    if INIT_DB:
+        interface.initialize_database()
 
 main()
